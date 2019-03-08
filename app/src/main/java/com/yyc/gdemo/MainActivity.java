53f3e0d06359046773d22e03ef8c909185be027e
yaoyongchao <yaoyongchao3g@163.com>
2019-03-08 14:34:35 +0800

    1111

--- app/src/main/java/com/yyc/gdemo/MainActivity.java
+++ app/src/main/java/com/yyc/gdemo/MainActivity.java
@@ -3,6 +3,12 @@ package com.yyc.gdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @Author Arno
+ * @Date 19-3-8 下午2:31
+ * @Description: 我是v1版本. 我是v2 发发送到发送到.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
